0ea04584909a260524fe70a53397cb0ae9915434
asdadsdasd <mkozarez12345@mail.ru>
2023-02-26 23:01:17 +0300

    My second commit to master

--- kozarez123.cpp
+++ kozarez123.cpp
@@ -97,7 +97,7 @@ int determineFunctionCreator(const char programCode[MAX_STR_COUNT][MAX_STR_LEN],
 
     return functionIsFound;
 }
-                                                                                                                                                                                
+My third change                                                                                                                                                                         
 
 PositionInCode findFunctionAndItTypeOfEntry(const char programCode[MAX_STR_COUNT][MAX_STR_LEN], int stringCount, const char functionName[], TypeOfEntryFunction* typeOfEntryFunction) {
     
