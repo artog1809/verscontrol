6c8f3aa5bbbdaf55770f773aef5cbdca3c9f262c
DmitryDemenkov <demon.krut2002@mail.ru>
2023-02-27 13:05:12 +0300

    add demenkov file

---
+++ demenkov.cpp
@@ -0,0 +1,89 @@
+#include "filemanager.h"
+
+/*!
+ * \file
+ * \brief Файл, содержащий реализацию FileManager
+ */
+
+FileManager::FileManager() { }
+
+bool FileManager::setInputFile(QString filename)
+{
+    ErrorController errorController;
+
+    bool isInputCorrect = errorController.isInputFileCorrect(filename);
+    if (isInputCorrect)
+        inputFilename = filename;
+
+    return isInputCorrect;
+}
+
+bool FileManager::setOutputFile(QString filename)
+{
+    ErrorController errorController;
+
+    bool isOutputCorrect = errorController.isOutputFileCorrect(filename);
+    if (isOutputCorrect)
+        outputFilename = filename;
+
+    return isOutputCorrect;
+}
+
+QDomNode FileManager::getXRootNode()
+{
+    QDomDocument doc(inputFilename);
+    QFile inputFile(inputFilename);
+
+    inputFile.open(QIODevice::ReadOnly);
+    doc.setContent(&inputFile);
+    inputFile.close();
+    QDomElement docElem = doc.documentElement();
+    QDomNode node = docElem.firstChild();
+
+    return node;
+}
+
+void FileManager::writeOutputFile(QStringList &subExpressions, short *values, int rowAmount, int columnAmount)
+{
+    QFile outputFile(outputFilename);
+    outputFile.open(QIODevice::WriteOnly);
+    QTextStream outputStream(&outputFile);
+
+    // Добавить строковые представления узлов в заголовки колонок
+    QString header = "";
+    for (int i = 0; i < columnAmount; i++)
+    {
+        header.append(subExpressions[i]);
+
+        if (i < columnAmount - 1)
+            header.append(";");
+        else
+            header.append("\n");
+    }
+
+    // Преобразовать строки матрицы значений в строки выходного файла
+    char* vals = (char*)malloc(2*rowAmount*columnAmount*sizeof(char)+1);
+
+    int k = 0;
+    for(int i = 0; i < rowAmount; i++)
+    {
+        for (int j = 0; j < columnAmount; j++, k+=2)
+        {
+            char val = *(values + i*columnAmount + j) + 48;
+
+            *(vals + k) = val;
+
+            if (j < columnAmount - 1)
+                *(vals + k+1) = ';';
+            else
+                *(vals + k+1) = '\n';
+        }
+    }
+    *(vals + k) = '\0';
+
+    outputStream << header;
+    outputStream << vals;
+    outputFile.close();
+    free(values);
+    free(vals);
+}
